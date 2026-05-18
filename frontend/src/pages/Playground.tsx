7daaf7e121c8a3432c226ab581168ee525e2a364
doneternity <doneternity10@gmail.com>
2026-05-18 22:43:34 +0200

    fix no-constant-condition lint error in playground

--- frontend/src/pages/Playground.tsx
+++ frontend/src/pages/Playground.tsx
@@ -73,6 +73,7 @@ async function streamCompletion(
     const dec = new TextDecoder();
     let buf = "";
 
+    // eslint-disable-next-line no-constant-condition
     while (true) {
       const { done, value } = await reader.read();
       if (done) break;
